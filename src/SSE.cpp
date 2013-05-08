681e08e60025752b61285b9edb84fa798db18a3f
Muhammad <mnaveed29@gmail.com>
2013-05-07 19:20:21 -0500

    BlindStorage working with SSE build

---
+++ src/SSE.cpp
@@ -0,0 +1,109 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <boost/filesystem.hpp>
+#include <boost/tokenizer.hpp>
+
+#include "SSE.h"
+
+using std::cout;
+using std::endl;
+using std::ifstream;
+
+docid_t SSE::docNameHash(string & doc)
+{
+	static CryptoPP::SHA256 hash;
+	static byte digest[CryptoPP::SHA256::DIGESTSIZE];
+	hash.CalculateDigest(digest, (unsigned char *) doc.c_str(), CryptoPP::SHA256::DIGESTSIZE);
+	docid_t result;
+	memcpy(&result, digest, sizeof(docid_t));
+	return result;
+}
+
+SSE::SSE()
+{
+	// TODO: construct and initialize BStore object
+}
+
+void SSE::indexGen(string path)
+{
+	cout << "[PATH] " << path << endl;
+
+	// OPTIONAL: Print progress
+	int fileSum = 0, fileCount = 0;
+	for(boost::filesystem::recursive_directory_iterator end, dir(path); dir != end; ++dir) {
+		if(boost::filesystem::is_regular(dir->status())) {
+			fileSum++;
+		}
+	}
+
+	for(boost::filesystem::recursive_directory_iterator end, dir(path); dir != end; ++dir) {
+//		string fileName = boost::filesystem::canonical(dir->path()).string();
+		string fileName = dir->path().string();
+		if(boost::filesystem::is_regular(dir->status())) {
+//			cout << "[FILE] " << fileName << endl;
+			fileCount++;
+			docid_t hash = docNameHash(fileName);
+			hash &= 0x7FFFFFFFFFFFFFFFL;
+			ifstream input(fileName.c_str());
+			string getcontent;
+			boost::tokenizer<> tok(getcontent);
+			while(getline(input, getcontent)) {
+				tok.assign(getcontent.begin(), getcontent.end());
+				for(boost::tokenizer<>::iterator beg = tok.begin(); beg != tok.end(); ++beg) {
+					string keyword(*beg);
+					// OPTIONAL: convert keywords to lower case
+					std::transform(keyword.begin(), keyword.end(), keyword.begin(), ::tolower);
+//					cout << "[KWRD] " << keyword << endl;
+					// Possible optimization: memorize hash of fileName
+					// add keyword --> fileName to the map
+					map[keyword].insert(hash);
+				}
+			}
+			input.close();
+		} else {
+			// it's a directory
+			cout << "[DIR] [" << (int)((double)fileCount/(double)fileSum*100) << "%] " << fileName << endl;
+		}
+	}
+	cout << "[DONE] Number of keywords: " << map.size() << std::endl;
+
+	// OPTIONAL: Estimate map size
+	uint64_t mapSize = 0;
+	for(unordered_map<string, unordered_set<docid_t> >::iterator itmap = map.begin(); itmap != map.end(); ++itmap) {
+		const string & keyword = itmap->first;
+		mapSize += keyword.size();
+		unordered_set<docid_t> & set = itmap->second;
+		for(unordered_set<docid_t>::iterator itset = set.begin(); itset != set.end(); ++itset) {
+			docid_t documentId = *itset;
+			mapSize += 8;
+		}
+	}
+	cout << "[DONE] Estimated map size: " << mapSize << endl;
+
+
+	// TODO: call BStore with the map
+	store = new BStore(map);
+
+	// TODO: copy all documents to DocumentStore, rename each file to its docId
+
+}
+
+void SSE::remove(string document)
+{
+	// TODO: delete docNameHash(document) from DocumentStore
+}
+
+void SSE::add(string path)
+{
+	// TODO
+}
+
+vector<docid_t> SSE::search(string keyword)
+{
+	// call BStore.get(keyword)
+	// convert byte array to array of uint64_t
+	// TODO: check for file with filename docNameHash(document) in
+}
+
---
+++ src/SSE.h
@@ -0,0 +1,46 @@
+#ifndef SSE_H_
+#define SSE_H_
+
+#include <vector>
+#include <iostream>
+//#include<ext/hash_map>
+//#include<ext/hash_set>
+#include <tr1/unordered_map>
+#include <tr1/unordered_set>
+#include <cryptopp/hex.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/base64.h>
+
+#include "AES.h"
+#include "BStore.h"
+
+using std::string;
+using std::pair;
+using std::istream;
+using std::vector;
+
+using std::tr1::unordered_map;
+using std::tr1::unordered_set;
+
+typedef uint64_t docid_t;
+
+class SSE {
+
+public:
+	SSE();
+//	virtual ~SSE();
+	void indexGen(string path);
+	void remove(string document);
+	void add(string path);
+	vector<docid_t> search(string keyword);
+
+private:
+	BStore* store;
+	docid_t docNameHash(string & doc);
+	// map from keywords to fileNames
+	unordered_map<string, unordered_set<docid_t> > map;
+	// TODO: have a BStore object inside
+	// TODO: function to index individual files (refactor indexGen)
+};
+
+#endif /* SSE_H_ */
