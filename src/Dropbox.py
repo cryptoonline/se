358e9ac2d24f8c0896af72448f5483c2ce9864d4
Muhammad <mnaveed29@gmail.com>
2013-07-11 20:22:23 -0400

    Improved Dropbox Interface

--- src/Dropbox.py
+++ src/Dropbox.py
@@ -6,106 +6,56 @@ Created on May 1, 2013
 
 import sys
 import cStringIO
+from dropbox import client, rest, session
 # Include the Dropbox SDK libraries
 
+class DropboxAPI:
+	client = __import__('dropbox.client')
+	def __init__(self):
+		# from dropbox import client, rest, session
+		# Get your app key and secret from the Dropbox developer website
+		APP_KEY = 'krrlaf6p6alrvwp'
+		APP_SECRET = 'wi5o59b5rwao11o'
 
-def upload(filename, fileContents):
-	from dropbox import client, rest, session
-#	if path=='/':
-#		path='';
+		# ACCESS_TYPE should be 'dropbox' or 'app_folder' as configured for your app
+		ACCESS_TYPE = 'dropbox'
 
-#	print (path)
-	print (filename)
-	print ('\n')
-	print (fileContents)
-	print ('\n')
-#	print '%s%s'%(path,filename)
-	# Get your app key and secret from the Dropbox developer website
-	APP_KEY = 'krrlaf6p6alrvwp'
-	APP_SECRET = 'wi5o59b5rwao11o'
+		sess = session.DropboxSession(APP_KEY, APP_SECRET, ACCESS_TYPE)
 
-	# ACCESS_TYPE should be 'dropbox' or 'app_folder' as configured for your app
-	ACCESS_TYPE = 'dropbox'
+		request_token = sess.obtain_request_token()
 
-	sess = session.DropboxSession(APP_KEY, APP_SECRET, ACCESS_TYPE)
+		url = sess.build_authorize_url(request_token)
 
-	request_token = sess.obtain_request_token()
+		# Make the user sign in and authorize this token
+		print "url:", url
+		print "Please visit this website and press the 'Allow' button, then hit 'Enter' here."
 
-	url = sess.build_authorize_url(request_token)
+		import webbrowser
+		new = 2 # open in a new tab, if possible
+		# open a public URL, in this case, the webbrowser docs
+		webbrowser.open(url,new=new)
 
-	# Make the user sign in and authorize this token
-	print "url:", url
-	print "Please visit this website and press the 'Allow' button, then hit 'Enter' here."
+		raw_input()
+		
+		# This will fail if the user didn't visit the above URL and hit 'Allow'
+		access_token = sess.obtain_access_token(request_token)
 
+		DropboxAPI.client = client.DropboxClient(sess)	
+		print "linked account:", DropboxAPI.client.account_info()
+	
+	def upload(self, filename, fileContents):
+		import filelike
+		from StringIO import StringIO as SIO
+		f = filelike.join([SIO(fileContents)])
+		response = DropboxAPI.client.put_file('/%s' %filename, f)
+		print "uploaded:", response
 
-	import webbrowser
-	new = 2 # open in a new tab, if possible
-	# open a public URL, in this case, the webbrowser docs
-	webbrowser.open(url,new=new)
+		folder_metadata = DropboxAPI.client.metadata('/')
+		# print "metadata:", folder_metadata
+		print "Uploaded\n"
+		return;
 
-	raw_input()
-
-	# This will fail if the user didn't visit the above URL and hit 'Allow'
-	access_token = sess.obtain_access_token(request_token)
-
-
-	client = client.DropboxClient(sess)
-	print "linked account:", client.account_info()
-#	full='%s%s'%(path,filename)
-#	print (full)
-#	f = open(full,'rb')
-#	f = cStringIO.StringIO()
-#	f.write(fileContents)
-#	print f.getvalue()
-	import filelike
-	from StringIO import StringIO as SIO
-	f = filelike.join([SIO(fileContents)])
-	response = client.put_file('/%s' %filename, f)
-	print "uploaded:", response
-
-	folder_metadata = client.metadata('/')
-	print "metadata:", folder_metadata
-	return;
-
-def download(filename, fileContents):
-	from dropbox import client, rest, session
-#	if path=='/':
-#		path='';
-	# Get your app key and secret from the Dropbox developer website
-	APP_KEY = 'krrlaf6p6alrvwp'
-	APP_SECRET = 'wi5o59b5rwao11o'
-
-	# ACCESS_TYPE should be 'dropbox' or 'app_folder' as configured for your app
-	ACCESS_TYPE = 'dropbox'
-
-	sess = session.DropboxSession(APP_KEY, APP_SECRET, ACCESS_TYPE)
-
-	request_token = sess.obtain_request_token()
-
-	url = sess.build_authorize_url(request_token)
-
-	# Make the user sign in and authorize this token
-	print "url:", url
-	print "Please visit this website and press the 'Allow' button, then hit 'Enter' here."
-
-
-	import webbrowser
-	new = 2 # open in a new tab, if possible
-	# open a public URL, in this case, the webbrowser docs
-	webbrowser.open(url,new=new)
-
-	raw_input()
-
-	# This will fail if the user didn't visit the above URL and hit 'Allow'
-	access_token = sess.obtain_access_token(request_token)
-
-
-	client = client.DropboxClient(sess)
-	print "linked account:", client.account_info()
-	f, metadata = client.get_file_and_metadata('/%s' %filename)
-#	out = open('%s%s'%(path,filename), 'wb')
-#	out.write(f.read())
-#	out.close()
-	print(metadata)
-#	print f.read()
-	return f.read()
+	def download(self, filename, fileContents):
+		f, metadata = DropboxAPI.client.get_file_and_metadata('/%s' %filename)
+		# print(metadata)
+		return f.read();
--- src/callPython.cpp
+++ src/callPython.cpp
@@ -6,92 +6,66 @@ using std::string;
 
 typedef unsigned char byte;
 
-int Dropbox_Interface(char*, char*, char*, char*, size_t);
+int Dropbox_Interface(char*, char*, char*, size_t);
 
 void upload(string filename, char* file, size_t size){
 	char filenameChars[filename.size()];
 	memcpy(filenameChars, filename.c_str(), filename.size());
-	Dropbox_Interface("Dropbox", "upload", filenameChars, file, size);
+	Dropbox_Interface("upload", filenameChars, file, size);
 }
 
 void download(string filename, char* file){
 	char filenameChars[filename.size()];
 	memcpy(filenameChars, filename.c_str(), filename.size());
-	Dropbox_Interface("Dropbox", "download", filenameChars, file, 0);	
+	Dropbox_Interface("download", filenameChars, file, 0);	
 }
 
-int Dropbox_Interface(char* pythonfile,  char* function, char* filename, char* fileContents, size_t size)
-{
-    PyObject *pName, *pModule, *pDict, *pFunc;
-    PyObject *pArgs, *pValue;
-    int i;
+int Dropbox_Interface(char* function, char* filename, char* fileContents, size_t size){
+	char pythonfile[] = "Dropbox";
+	char pythonClass[] = "DropboxAPI";
+		
+	PyObject *pName, *pModule, *pDict, *pFunc;
+	PyObject *pClass, *pArgs, *pValue;
+	PyObject *pInstance;
+	int i;
 
-    Py_Initialize();
-    pName = PyString_FromString(pythonfile);
-    /* Error checking of pName left out */
+	Py_Initialize();
+	pName = PyString_FromString(pythonfile);
+	/* Error checking of pName left out */
 
-//		PySys_SetPath("."); 
+	pModule = PyImport_Import(pName);
 
-    pModule = PyImport_Import(pName);
-    Py_DECREF(pName);
+	//pDict is borrowed reference
+	pDict = PyModule_GetDict(pModule);
+	
+	//Build the name of a callable class
+	pClass = PyDict_GetItemString(pDict, pythonClass);
 
-    if (pModule != NULL) {
-        pFunc = PyObject_GetAttrString(pModule, function);
-        /* pFunc is a new reference */
-				int numArgs = 2;
-        if (pFunc && PyCallable_Check(pFunc)) {
-            pArgs = PyTuple_New(numArgs);
-            for (i = 0; i < numArgs; ++i) {
-			  			if(i == 0)
-								pValue = PyString_FromString(filename);
-							else if (i == 1)
-								pValue = PyString_FromString(fileContents);
-	//						else if (i == 2)
-		//						pValue = PyInt_FromLong(size);
-              if (!pValue) {
-                    Py_DECREF(pArgs);
-                    Py_DECREF(pModule);
-                    fprintf(stderr, "Cannot convert argument\n");
-                    return 1;
-                }
-                /* pValue reference stolen here: */
-                PyTuple_SetItem(pArgs, i, pValue);
-            }
-            pValue = PyObject_CallObject(pFunc, pArgs);
-			if(function == "download"){
-				PyObject* objectsRepresentation = PyObject_Repr(pValue);
-				const char* downloadedFile = PyString_AsString(objectsRepresentation);
-				memcpy(fileContents, downloadedFile, sizeof(downloadedFile));
-				printf("%s", downloadedFile);
-			}
-            Py_DECREF(pArgs);
-            if (pValue != NULL) {
-                printf("Result of call: %ld\n", PyInt_AsLong(pValue));
-                Py_DECREF(pValue);
-            }
-            else {
-                Py_DECREF(pFunc);
-                Py_DECREF(pModule);
-                PyErr_Print();
-                fprintf(stderr,"Call failed\n");
-                return 1;
-            }
-        }
-        else {
-            if (PyErr_Occurred())
-                PyErr_Print();
-            fprintf(stderr, "Cannot find function \"%s\"\n", function);
-        }
-        Py_XDECREF(pFunc);
-        Py_DECREF(pModule);
-    }
-    else {
-        PyErr_Print();
-        fprintf(stderr, "Failed to load \"%s\"\n", pythonfile);
-        return 1;
-    }
-    Py_Finalize();
-		return 0;
+	//Create an instance of the class
+	if(PyCallable_Check(pClass))
+		pInstance = PyObject_CallObject(pClass, NULL);
+
+	pValue = PyObject_CallMethod(pInstance, function, "(ss)", filename, fileContents);
+
+	if(function == "download"){
+		PyObject* objectsRepresentation = PyObject_Repr(pValue);
+		const char* downloadedFile = PyString_AsString(objectsRepresentation);
+		memcpy(fileContents, downloadedFile, sizeof(downloadedFile));
+		printf("%s", downloadedFile);
+	}
+	
+	if(pValue != NULL){
+		printf("Return of call : %d\n", PyInt_AsLong(pValue));
+		Py_DECREF(pValue);
+	}
+	else{
+		PyErr_Print();
+	}
+
+	Py_DECREF(pModule);
+	Py_DECREF(pName);
+	Py_Finalize();
+	return 0;
 }
 
 int main(){
